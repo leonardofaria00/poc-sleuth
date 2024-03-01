c2d8668cd934a04b46e76bda7ce427f17700a2dd
LEONARDO FARIA DOS SANTOS <leonardofaria00@gmail.com>
2024-03-01 00:46:55 -0300

    Created API exemple with sleuth trace id

---
+++ src/main/java/com/example/pocsleuth/infrastructure/integration/data/ClientResponse.java
@@ -0,0 +1,14 @@
+package com.example.pocsleuth.infrastructure.integration.data;
+
+public class ClientResponse {
+
+  private String message;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(final String message) {
+    this.message = message;
+  }
+}
