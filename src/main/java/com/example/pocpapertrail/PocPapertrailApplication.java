e0c0e5769f3f06c40770dbd6568a42781eae16ac
Leonardo Faria <leonardo.faria@zallpy.com>
2024-02-29 21:10:20 -0300

    Initial project

---
+++ src/main/java/com/example/pocpapertrail/PocPapertrailApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocpapertrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocPapertrailApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PocPapertrailApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/pocpapertrail/PocPapertrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocpapertrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocPapertrailApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
